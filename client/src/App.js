dc600afb1060ef03d9dc474d6ef303eca9a4eb46
Kaytee07 <kofi.taylor07@gmail.com>
2022-01-13 19:17:13 +0000

    to do list is complete with useReducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React, {useReducer, useState} from 'react'
 import './App.css';
+import Todo from './todo';
+
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
+}
+
+const reducer = (todos, action) => {
+  if(action.type === ACTIONS.ADD_TODO){
+    return [...todos, newTodo(action.payload.name)]
+  }else if(action.type === ACTIONS.TOGGLE_TODO){
+    return todos.map(todo => {
+      if(todo.id === action.payload.id){
+        return {...todo, complete:!todo.complete}
+      }
+      return todo
+    })
+  }else if(action.type === ACTIONS.DELETE_TODO){
+    return todos.filter(todo=> todo.id !== action.payload.id)
+    
+  }
+}
+
+function newTodo(name){
+  return { id:Date.now(), name:name, complete:false }
+}
 
 function App() {
+const [todos, dispatch] = useReducer(reducer, [])
+const [name, setName] = useState("")
+const handleSubmit = (e) => {
+  e.preventDefault();
+  dispatch({type:ACTIONS.ADD_TODO, payload:{name:name}})
+  setName("");
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+    {todos.map(todo=>{
+     return <Todo key={todo.id} todo={todo} dispatch={dispatch}/>
+    })
+
+    }
+    </>
   );
 }
 
